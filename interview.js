bb193438732fbef236a6be7f6d53668c275e50a4
Neil Turner <n.turner@sap.com>
2019-10-25 19:08:47 -0700

    fixing logic

--- interview.js
+++ interview.js
@@ -2,25 +2,28 @@
 let LinkedList = function(node) {
   this.head = node
   this.tail = node
+  this.searchNode = this.head
+  this.offset = 0
 } 
 
-LinkedList.prototype.nextChar = function(node, offset) {
-  let currentNode = node
-  let end = node.value.length-1
-  
-  if(offset == end) {
-    currentNode = currentNode.next
-    end = currentNode.value.length
-    offset = 0
-  } else {
-    if(currentNode.value.charAt(offset) == '') {
-      return {char: '', offset: 0, currentNode: null}
-    }
-    char = currentNode.value.charAt(offset)
-    offset++
-   }
+LinkedList.prototype.nextChar = function() {
+  if(this.searchNode == null) {
+    return ''
+  }
 
-  return {char, offset, currentNode}
+  let end = this.searchNode.value.length-1
+  let char = ''
+  
+  if (this.offset === end) {
+    char = this.searchNode.value.charAt(end)
+    this.offset = 0
+    this.searchNode = this.searchNode.next
+  } else {
+    char = this.searchNode.value.charAt(this.offset)
+    this.offset++
+  }
+
+  return char
 }
 
 LinkedList.prototype.add = function(value) {
@@ -33,22 +36,24 @@ LinkedList.prototype.add = function(value) {
 }
 
 LinkedList.prototype.search = function(text) {
-  let scanned = this.nextChar(this.head, 0)
-  let target = text.slice(0, 1)
+  this.searchNode = this.head
+  this.offset = 0
+  let scanned = this.nextChar()
+  let target = text.charAt(0)
   
   let found = false
-  while(scanned.char != '') {
-    if(target === scanned.char) {
+  while(scanned != '') {
+    if(target === scanned) {
       found = true
     }
 
     if (found) {
       for(let i = 1; i < text.length; i++) {
-        scanned = this.nextChar(scanned.currentNode, scanned.offset)
-        if(scanned.char == '') {
+        scanned = this.nextChar()
+        if(scanned == '') {
           return false
         }
-        if(scanned.char != text[i]) {
+        if(scanned != text[i]) {
           found = false
           break
         }
@@ -57,7 +62,7 @@ LinkedList.prototype.search = function(text) {
         }
       }
     } else {
-      scanned = this.nextChar(scanned.currentNode, scanned.offset)
+      scanned = this.nextChar()
     }
   }
 
@@ -74,11 +79,10 @@ myList.add('o My na')
 myList.add('me is N')
 myList.add('eil. Nice to meet you can I tell you about the weather how is your family mine is great')
 
-// console.log(myList)
-// console.log(myList.search('eet yo'))
-// console.log(myList.search('how'))
+console.log(myList)
+console.log(myList.search('eet yo'))
+console.log(myList.search('how'))
 console.log(myList.search('lo'))
-
-
-
-
+console.log(myList.search('zzz'))
+console.log(myList.search('jksahdHello My name is Neil. Nice to meet you can I tell you about the weather how is your family mine is great'))
+console.log(myList.search('jksahdHello My name is Neil. Nice to meet you can I tell you about the weather how is your family mine is great!'))
